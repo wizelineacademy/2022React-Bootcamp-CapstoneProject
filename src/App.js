4cae76f520e4e570d20498780c299f76b53d0a57
Obed Vega <vega.obed@gmail.com>
2022-03-07 11:27:20 -0800

    deliverable 1 /  UI layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import './components/css/navbar.css';
+import './components/css/footer.css';
 import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
+import { NavBar } from './components/navbar';
+import { BodyDiv } from './components/body';
+import { FootDiv } from './components/footer';
 
 function App() {
   const { data, isLoading } = useFeaturedBanners();
@@ -8,20 +12,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <BodyDiv />
+      <FootDiv /> 
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export const NavBar = () => {
+  return (
+<div className="topnav">
+  <a className="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <div className="search-container">
+    <form action="/">
+      <input type="text" placeholder="Search.." name="search"/>
+	    <button type="submit"><i className="fa fa-shopping-cart"></i></button>
+      <button type="submit"><i className="fa fa-search"></i></button>
+    </form>
+  </div>
+</div>
+  );
+};
