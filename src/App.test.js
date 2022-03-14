e7fdae9d4d17aa00f53baa2e3236342f3f456ed6
Lucy <luceroadlt@gmail.com>
2022-03-13 23:34:43 -0600

    Update tes.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render Header correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText(/Lucy's Furniture Store/i);
+  expect(header).toBeInTheDocument();
 });
