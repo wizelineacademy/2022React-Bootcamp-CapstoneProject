d06fda9c5f21efe42909ee0e77c7a2d22055f061
Silvia rodriguez <silvideastora18@gmail.com>
2022-06-06 01:06:11 -0500

    Created Slideshow logic

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import Header from './components/Header/Header';
 import Slideshow from './components/Slideshow/Slideshow';
 import Footer from './components/Footer/Footer';
 import './App.css';
+import mock1 from './assets/mocks/es-mx/featured-banners.json';
 import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
 
 function App() {
   const { data, isLoading } = useFeaturedBanners();
   console.log(data, isLoading);
-
+  const banners = mock1.results.map(result => result.data.main_image.url)
   return (
     <div className="App">
       <Header/>
-      <Slideshow/>
+      <Slideshow banners={banners}/>
       <Footer/>
     </div>
   );
--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {MainContainer, SlideshowContainer, Slide, Controls, BotonSlide } from './Slideshow.styled';
-import img1 from '../../assets/sofa.jpg';
 import {ReactComponent as RightArrow} from '../../assets/right-arrow.svg';
 import {ReactComponent as LeftArrow} from '../../assets/left-arrow.svg';
 
     
-export default function Slideshow() {
+export default function Slideshow({banners}) {
+    const [ currentImg, setNextImage] = useState(0);
+
+
+
+    const nextBanner = () => {
+        setNextImage(currentImg + 1)
+    }
+    useEffect(() => {
+        if(currentImg === 3) {
+            setNextImage(0)
+        } else if (currentImg === -1){
+            setNextImage(2)
+        }
+    }, [currentImg])
+
+    const prevBanner = () => {
+        setNextImage(currentImg - 1)
+    } 
+    
 
     return (
         <MainContainer>
             <SlideshowContainer>
                 <Slide>
                     <a href=".">
-                        <img src={img1} alt=""/>
+                        <img src={banners[currentImg]} alt=""/>
                     </a>
                     <div>
                         <p>15% de descuento en productos de madera</p>
@@ -20,10 +38,10 @@ export default function Slideshow() {
                 </Slide>    
             </SlideshowContainer>
             <Controls>
-                <BotonSlide right>
-                    <RightArrow/>
+                <BotonSlide right onClick={nextBanner}>
+                    <RightArrow />
                 </BotonSlide>
-                <BotonSlide>
+                <BotonSlide left onClick={prevBanner}>
                     <LeftArrow/>
                 </BotonSlide>
             </Controls>
