2ddc9e8bcd4046e564bc3f748ac0d933d77ee27c
G-Linares <gerrylinares@gmail.com>
2022-06-06 10:13:41 -0500

    Initial commit with blank project to start modifications

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
 
@@ -8,20 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Esto es una prueba
     </div>
   );
 }
