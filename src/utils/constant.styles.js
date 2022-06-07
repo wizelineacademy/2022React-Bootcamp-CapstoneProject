77ce020cfc10f9e0b743c6700931c41c15ecc940
DavidTG7 <davidthekoa@gmail.com>
2022-06-06 20:09:45 -0500

    Constant style file

---
+++ src/utils/constant.styles.js
@@ -0,0 +1,5 @@
+export const bluePrince = '#065471';
+export const white = '#FFF';
+export const black = '#000';
+export const yellowMain = '#FCC444';
+export const appleGreenBlackground = '#24535c33';
