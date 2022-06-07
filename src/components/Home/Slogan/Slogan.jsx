53eff2e83c4cd05779ca16639b26c1bb54d84547
DavidTG7 <davidthekoa@gmail.com>
2022-06-06 19:46:32 -0500

    JSX file for Slogan component

---
+++ src/components/Home/Slogan/Slogan.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SloganFurny from './SloganFurny.styled';
+
+function Slogan() {
+  return (
+    <SloganFurny>
+      <h1>Make your stylish</h1>
+    </SloganFurny>
+  );
+}
+
+export default Slogan;
