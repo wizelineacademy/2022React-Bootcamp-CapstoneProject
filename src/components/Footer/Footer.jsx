36c9d503c6cc1feaea4aa350b0f573bf5054eab6
DavidTG7 <davidthekoa@gmail.com>
2022-06-06 20:03:18 -0500

    JSX file for Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FooterMain from './FooterMain.styled'
+
+function Footer() {
+  return (
+    <FooterMain>
+      Ecommerce created during Wizeline’s Academy React Bootcamp
+    </FooterMain>
+  );
+}
+
+export default Footer;
