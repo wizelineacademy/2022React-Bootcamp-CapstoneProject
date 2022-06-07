cbac110bc3572c54e3785d67fc9c60178e0a0af4
DavidTG7 <davidthekoa@gmail.com>
2022-06-06 20:05:22 -0500

    General Index file for all main components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from './Header';
+// import Slider from './Slider';
+// import Slogan from './Slogan';
+import Footer from './Footer';
+// import ProductCategories from './ProductCategories';
+// import FeaturedProducts from './FeaturedProducts';
+import Home from './Home';
+
+export { 
+  Header,
+  Home,
+  // Slider,
+  // Slogan,
+  Footer,
+  // ProductCategories,
+  // FeaturedProducts, 
+};
