308ec1aecb23499ce928e27fc519130fb2eeb2c0
DavidTG7 <davidthekoa@gmail.com>
2022-06-09 23:32:45 -0500

    Update constatn styles

--- src/utils/constant.styles.js
+++ src/utils/constant.styles.js
@@ -2,4 +2,5 @@ export const bluePrince = '#065471';
 export const white = '#FFF';
 export const black = '#000';
 export const yellowMain = '#FCC444';
-export const appleGreenBlackground = '#24535c33';
+export const appleGreenBackground = '#24535c33';
+export const whiteApple = 'rgba(250,250,250,.4)';
