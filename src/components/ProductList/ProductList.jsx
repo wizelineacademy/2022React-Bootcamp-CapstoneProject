0569f147045061dfefc165572f47fdd8102dc30f
Silvia rodriguez <silvideastora18@gmail.com>
2022-06-13 16:26:44 -0500

    Selected category logic fixed

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,25 +1,40 @@
-import {React, useState}  from 'react'
+import {React, useState, useEffect}  from 'react'
 import {ProductListContainer,SidebarWrapper,ProductCard,PaginationList} from './ProductList.styled';
 import mockProducts from '../../assets/mocks/en-us/product-categories.json';
 import {Card, CardText} from '../../components/Products/Products.styled';
 import mock3 from '../../assets/mocks/en-us/featured-products.json';
 
 export default function ProductList() {
-    const [selectedCategory, setSelectedCategory]= useState();
+    const [selectedCategory, setSelectedCategory]= useState([]);
     const [products, setProducts] = useState([]);
     
     const clickHandler = (categoryName) => {
-        setSelectedCategory(categoryName)
-        setProducts(mock3.results.filter(
-            product => product.data.category.slug === categoryName))
+        setSelectedCategory(prevValue => [...prevValue, categoryName])
+        
     }
+    useEffect(() => {
+        setProducts(mock3.results.filter(
+            product => {
+                let result = false;
+                for(let i = 0; i < selectedCategory.length; i++) {
+                    if(selectedCategory[i]=== product.data.category.slug){
+                        result = true;
+                        break;
+                    }
+                }
+                return result;
+            }))
+    }, [selectedCategory])
+
     return (
         <ProductListContainer>
             <SidebarWrapper>
                 {mockProducts.results.map((category,index) => {
                     return <ol 
                     key={category.data.name}
-                    className={selectedCategory === category.slugs[0] ? "is-active": ""} 
+                    className={
+                        selectedCategory.some((element)=> element === category.slugs[0])  
+                        ? "is-active": ""} 
                     onClick={e => clickHandler(category.slugs[0])}>{category.data.name}</ol>
                 })}; 
             </SidebarWrapper>
