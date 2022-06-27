3378536ca03dcce6af4b492a570a6db6dbb04b96
DavidTG7 <davidthekoa@gmail.com>
2022-06-26 22:36:21 -0500

    Update React Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App';
 import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
