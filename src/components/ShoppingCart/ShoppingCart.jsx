a255bfc193167f33131dbd13ad8f3110beb5e71c
Silvia rodriguez <silvideastora18@gmail.com>
2022-07-04 22:06:16 -0500

    Shopping cart subtotal fixed

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -6,18 +6,29 @@ import { Link } from 'react-router-dom';
 export default function ShopingCart() {
   const { items } = useContext(ShoppingCartContext);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [productsSubTotal, setProductsSubTotal] = useState({});
 
   useEffect(() => {
-    const total = items.reduce((total, item) => {
-      return total + item.total;
+    const total = Object.values(productsSubTotal).reduce((total, subTotal) => {
+      return total + subTotal;
     }, 0);
     setTotalPrice(total);
-  }, [items]);
+  }, [productsSubTotal]);
+
+  const calculateTotalByProduct = (productId, quantity, price) => {
+    setProductsSubTotal(prev => ({...prev, 
+      [productId]: quantity * price,
+    }))
+  }
+
 
   return (
     <ShoppingCartWrapper>
       {items.map((item, index) => (
-        <CartItemDetail item={item} index={index} />
+        <CartItemDetail 
+        item={item} 
+        index={index} 
+        calculateTotalByProduct={calculateTotalByProduct} />
       ))}
       <UiPannel>
         <div className="summary">
@@ -35,7 +46,7 @@ export default function ShopingCart() {
     </ShoppingCartWrapper>
   );
 }
-const CartItemDetail = ({ item, index }) => {
+const CartItemDetail = ({ item, index , calculateTotalByProduct}) => {
   const {
     data: {
       name,
@@ -45,6 +56,7 @@ const CartItemDetail = ({ item, index }) => {
       sku,
       stock,
     },
+    id,
     selectedQuantity,
   } = item;
   const [quantity, setQuantity] = useState(selectedQuantity || 1);
@@ -58,7 +70,7 @@ const CartItemDetail = ({ item, index }) => {
       setQuantity(quantity - 1);
     }
   };
-  useEffect(() => {
+  /*useEffect(() => {
     const newItems = [...items];
     newItems[index] = {
       ...item,
@@ -66,7 +78,13 @@ const CartItemDetail = ({ item, index }) => {
       total: quantity * price,
     };
     updateItems(newItems);
-  }, [quantity]);
+  }, [quantity]);*/
+
+  useEffect(() => {
+    calculateTotalByProduct(id, quantity, price)
+  },[quantity])
+  
+
   return (
     <CartItem key={sku}>
       <figure className="item-image">
